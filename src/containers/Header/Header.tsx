4ddbe68ec7d020c3261bde8bef4ff8ae01ac1226
datnt-fe <datnt.fe@gmail.com>
2023-02-22 23:57:53 +0700

    menu

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
 
 import { Link, navigate, useLocation } from '@reach/router';
@@ -8,7 +8,7 @@ import ImageVietnamFlag from '@/assets/images/image-vietnam-flag.png';
 import Button from '@/components/Button';
 import Avatar from '@/components/Avatar';
 import DropdownMenu from '@/components/DropdownMenu';
-
+import { useOnClickOutside } from 'usehooks-ts';
 import { dataHeaderNav } from './Header.data';
 import { THeaderProps } from './Header.types.d';
 import './Header.scss';
@@ -23,12 +23,19 @@ import { ETypeNotification } from '@/common/enums';
 const Header: React.FC<THeaderProps> = () => {
   const dispatch = useDispatch();
   const { pathname } = useLocation();
+  const [open, setOpen] = useState(false);
+  const ref = useRef(null);
   const isMobile = useSelector((state: TRootState) => state.uiReducer.device.isMobile);
   const atk = AuthHelpers.getAccessToken();
   const handleLogout = (): void => {
     dispatch(authLogoutAction.request({}, handleAuthLogoutSuccess));
   };
-
+  const openMenuToggle = (): void => {
+    setOpen(!open);
+  };
+  const closeMenu = (): void => {
+    setOpen(false);
+  };
   const handleAuthLogoutSuccess = (): void => {
     AuthHelpers.clearTokens();
     dispatch(getProfileAction.success(undefined));
@@ -80,80 +87,117 @@ const Header: React.FC<THeaderProps> = () => {
   useEffect(() => {
     getProfile();
   }, [getProfile]);
+  useOnClickOutside(ref, closeMenu);
   return (
-    <header className={classNames('Header')}>
-      <div className="container">
-        <div className="Header-wrapper flex items-center justify-between">
-          {isMobile ? (
-            <>
-              <div className="Header-wrapper-toggle">
-                <Icon name={EIconName.Bars} />
-              </div>
-            </>
-          ) : (
-            ''
-          )}
-          {!isMobile ? (
-            <Link className="Header-logo" to={Paths.Home}>
-              <img src={Logo} alt="" />
-            </Link>
-          ) : (
-            <nav className="Header-nav">
-              <ul className="Header-nav-list flex items-center">
-                {dataHeaderNav.map((item) => (
-                  <li className="Header-nav-list-item">
-                    {item.hash && pathname === Paths.Home ? (
-                      <a href={item.hash}>{item.title}</a>
-                    ) : (
-                      <Link to={item.link}>{item.title}</Link>
-                    )}
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          )}
-
-          {!isMobile ? (
-            <nav className="Header-nav">
-              <ul className="Header-nav-list flex items-center">
-                {dataHeaderNav.map((item) => (
-                  <li className="Header-nav-list-item">
-                    {item.hash && pathname === Paths.Home ? (
-                      <a href={item.hash}>{item.title}</a>
-                    ) : (
-                      <Link to={item.link}>{item.title}</Link>
-                    )}
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          ) : (
-            <Link className="Header-logo" to={Paths.Home}>
-              <img src={Logo} alt="" />
-            </Link>
-          )}
-
-          <div className="Header-actions flex items-center">
-            {!profileState ? (
-              <div className="Header-login">
-                <Button title="Đăng nhập" type="primary" size="large" link={`${LayoutPaths.Auth}${Paths.Login}`} />
-              </div>
-            ) : (
+    <>
+      {isMobile ? (
+        <>
+          <nav className={`Header-nav-mobile ${open ? 'open' : ''}`} ref={ref}>
+            <div className="Header-wrapper-toggle-close" onClick={openMenuToggle}>
+              <Icon name={EIconName.Close} />
+            </div>
+            <ul className="Header-nav-list flex">
+              {dataHeaderNav.map((item) => (
+                <li className="Header-nav-list-item">
+                  {item.hash && pathname === Paths.Home ? (
+                    <a href={item.hash}>{item.title}</a>
+                  ) : (
+                    <Link to={item.link}>{item.title}</Link>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div className="overlay-menu" style={{ 'display': open ? 'block' : 'none' }} />
+        </>
+      ) : (
+        ''
+      )}
+      <header className={classNames('Header')}>
+        <div className="container">
+          <div className="Header-wrapper flex items-center justify-between">
+            {isMobile ? (
               <>
-                <div className="Header-language">
-                  <img src={ImageVietnamFlag} alt="" />
-                </div>
-                <div className="Header-account">
-                  <DropdownMenu options={dataAccountMenu} minWidth="24rem" placement="bottomRight">
-                    <Avatar image={process.env.REACT_APP_SERVICE_BASE_URL + profileState?.user.avatar} size={40} />
-                  </DropdownMenu>
+                <div className="Header-wrapper-toggle" onClick={openMenuToggle}>
+                  <Icon name={EIconName.Bars} />
                 </div>
               </>
+            ) : (
+              ''
             )}
+            {!isMobile ? (
+              <Link className="Header-logo" to={Paths.Home}>
+                <img src={Logo} alt="" />
+              </Link>
+            ) : (
+              <nav className="Header-nav">
+                <ul className="Header-nav-list flex items-center">
+                  {dataHeaderNav.map((item) => (
+                    <li className="Header-nav-list-item">
+                      {item.hash && pathname === Paths.Home ? (
+                        <a href={item.hash}>{item.title}</a>
+                      ) : (
+                        <Link to={item.link}>{item.title}</Link>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            )}
+
+            {!isMobile ? (
+              <nav className="Header-nav">
+                <ul className="Header-nav-list flex items-center">
+                  {dataHeaderNav.map((item) => (
+                    <li className="Header-nav-list-item">
+                      {item.hash && pathname === Paths.Home ? (
+                        <a href={item.hash}>{item.title}</a>
+                      ) : (
+                        <Link to={item.link}>{item.title}</Link>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            ) : (
+              <Link className="Header-logo" to={Paths.Home}>
+                <img src={Logo} alt="" />
+              </Link>
+            )}
+
+            <div className="Header-actions flex items-center">
+              {!profileState ? (
+                pathname === '/auth/login' ? (
+                  <div className="Header-login">
+                    <Button
+                      title="Tạo tài khoản"
+                      type="primary"
+                      size="large"
+                      link={`${LayoutPaths.Auth}${Paths.Login}`}
+                    />
+                  </div>
+                ) : (
+                  <div className="Header-login">
+                    <Button title="Đăng nhập" type="primary" size="large" link={`${LayoutPaths.Auth}${Paths.Login}`} />
+                  </div>
+                )
+              ) : (
+                <>
+                  <div className="Header-language">
+                    <img src={ImageVietnamFlag} alt="" />
+                  </div>
+                  <div className="Header-account">
+                    <DropdownMenu options={dataAccountMenu} minWidth="24rem" placement="bottomRight">
+                      <Avatar image={process.env.REACT_APP_SERVICE_BASE_URL + profileState?.user.avatar} size={40} />
+                    </DropdownMenu>
+                  </div>
+                </>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
 
